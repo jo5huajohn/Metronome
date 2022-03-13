2d8344bc8658304503726b5ab29dcc5140584fe6
Joshua John <joshuajohnv@protonmail.com>
2022-03-13 13:52:09 +0530

    - Added limit checking for minimum values - Added logic to handle cases where a floating point number is passed - Fixed issue where bpm < 60 cause nanosleep to fail - Fixed issue where program would fail with segmentation fault when   default values were passed.
    Note: As of now, the value of beats per measure is accepted but not used.
          This will change in the future.

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 A standard metronome written in C.
 
 Arguments to pass:
-- Time signature (e.g: 4/4)
+- Time signature (e.g: 4/4, Limited to 16/8) 
 - BPM (Limited to 400 bpm)
-- Subdivision
+- Subdivision (Limited to 32)
   - quarter notes - 4
   - eighth notes - 8
   - sixteenth notes - 16
--- metronome.c
+++ metronome.c
@@ -7,11 +7,17 @@
 
 #define DEFAULT_BPM             60
 #define DEFAULT_SUB_DIV         4
-#define DEFAULT_TIME_SIGNATURE  "4/4"
+
 #define MAX_BPM                 400
 #define MAX_NO_OF_BEATS         16
 #define MAX_SUB_DIV             32
 #define MAX_VALUE_OF_BEATS      8
+
+#define MIN_BPM                 20
+#define MIN_NO_OF_BEATS         1
+#define MIN_SUB_DIV             4
+#define MIN_VALUE_OF_BEATS      1
+
 #define ONE_MIN_IN_NANOSECS     6e10
 #define ONE_SEC_IN_NANOSECS     1e9
 #define SUB_DIV_DIVIDER         4
@@ -20,18 +26,22 @@ int8_t metronome(char *time_signature, uint16_t bpm, uint8_t sub_div);
 
 int main(int argc, char *argv[])
 {
+    char default_time_signature[] = "4/4";
     int8_t res = 0;
     uint8_t sub_div = DEFAULT_SUB_DIV;
     uint16_t bpm = DEFAULT_BPM;
-    char *time_signature = DEFAULT_TIME_SIGNATURE;
-
-    if (argc == 4)
+    char *time_signature = default_time_signature;
+    
+    if (argc != 1)
     {
-        sub_div = atoi(argv[3]);
-    }
+        if (argc == 4)
+        {
+            sub_div = (uint8_t)atof(argv[3]);
+        }
 
-    bpm = atoi(argv[2]);
-    time_signature = argv[1];
+        bpm = (uint16_t)atof(argv[2]);
+        time_signature = argv[1];
+    }
 
     res = metronome(time_signature, bpm, sub_div);
 
@@ -40,24 +50,45 @@ int main(int argc, char *argv[])
 
 int8_t metronome(char *time_signature, uint16_t bpm, uint8_t sub_div)
 {
-    if (bpm > MAX_BPM)
+    if (bpm < MIN_BPM)
+    {
+        printf("BPM too low\n");
+
+        return -1;
+    }
+
+    else if (bpm > MAX_BPM)
     {
         printf("BPM too high\n");
 
         return -1;
     }
 
-    if (sub_div > MAX_SUB_DIV)
+    if (sub_div < MIN_SUB_DIV)
+    {
+        printf("Not enough sub-divisions\n");
+
+        return -1;
+    }
+    
+    else if (sub_div > MAX_SUB_DIV)
     {
         printf("Too many sub-divisions\n");
 
         return -1;
     }
 
-    uint8_t top = atoi(strtok(time_signature, "/"));
-    uint8_t bottom = atoi(strtok(NULL, "/"));
+    uint8_t top = (uint8_t)atof(strtok(time_signature, "/"));
+    uint8_t bottom = (uint8_t)atof(strtok(NULL, "/"));
 
-    if ((top > MAX_NO_OF_BEATS) || (bottom > MAX_VALUE_OF_BEATS))
+    if ((top < MIN_NO_OF_BEATS) || (bottom < MIN_VALUE_OF_BEATS))
+    {
+        printf("Time signature is too high\n");
+
+        return -1;
+    }
+
+    else if ((top > MAX_NO_OF_BEATS) || (bottom > MAX_VALUE_OF_BEATS))
     {
         printf("Time signature is too high\n");
 
@@ -70,9 +101,10 @@ int8_t metronome(char *time_signature, uint16_t bpm, uint8_t sub_div)
 
     memset(&tim, 0, sizeof(tim));
 
-    if (tick == ONE_SEC_IN_NANOSECS)
+    if (tick >= ONE_SEC_IN_NANOSECS)
     {
         tim.tv_sec = 1;
+        tim.tv_nsec = tick - ONE_SEC_IN_NANOSECS;
     }
 
     else
